5fee63743c510ee380021ccc2b672333484c8f68
mk2b79 <samchuk.maks@outlook.com>
2024-11-19 14:10:30 +0200

    update dependency

--- Wallet.Wise.WebApp/Program.cs
+++ Wallet.Wise.WebApp/Program.cs
@@ -1,12 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Wallet.Wise.BLL.Services;
+using Wallet.Wise.Common.Mappings;
 using Wallet.Wise.DAL.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+//Add DbContext Injection
 builder.Services.AddDbContext<WalletWiseContext>(options => options.UseSqlite("Data source=../WalletWiseDb.sql"));
+//Add AutoMapper Injection
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+//BLL Services Injection
 builder.Services.AddScoped<CategoryServices>();
 
 //Pipe-line http request
