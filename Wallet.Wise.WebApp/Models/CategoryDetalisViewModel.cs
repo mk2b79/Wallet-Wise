70a68095b7026f4c0b469d3e3c2ad956ca8e442c
mk2b79 <samchuk.maks@outlook.com>
2024-11-19 14:09:15 +0200

    delete viewModel

--- Wallet.Wise.WebApp/Models/CategoryDetalisViewModel.cs
+++
@@ -1,9 +0,0 @@
-using Wallet.Wise.DAL.Entities;
-
-namespace Wallet.Wise.WebApp.Models;
-
-public class CategoryDetalisViewModel
-{
-    public Category Category { get; set; }
-    public IEnumerable<Record> Records { get; set; }
-}
