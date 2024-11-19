9365031a85c6756a7fa6757fc986d0f2f0c6f653
mk2b79 <samchuk.maks@outlook.com>
2024-11-19 14:08:58 +0200

    update CategoryServices

--- Wallet.Wise.BLL/Services/CategoryServices.cs
+++ Wallet.Wise.BLL/Services/CategoryServices.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Wallet.Wise.BLL.DTOs;
 using Wallet.Wise.BLL.Services.IServices;
 using Wallet.Wise.DAL.Context;
 using Wallet.Wise.DAL.Entities;
@@ -8,9 +10,11 @@ namespace Wallet.Wise.BLL.Services;
 public class CategoryServices:ICategoryServices
 {
     private readonly WalletWiseContext _context;
-    public CategoryServices(WalletWiseContext context)
+    private readonly IMapper _mapper;
+    public CategoryServices(WalletWiseContext context, IMapper mapper)
     {
         _context = context;
+        _mapper = mapper;
     }
 
     public async Task<IEnumerable<Category>> GetAllAsync()
@@ -18,11 +22,19 @@ public class CategoryServices:ICategoryServices
         return await _context.Categories.ToListAsync();
     }
     
-    public async Task<IEnumerable<Category>> GetAllByMountAsync(DateTime mount)
+    //public async Task<IEnumerable<Category>> GetAllByMountAsync(DateTime mount)
+    //{
+    //    return await _context.Categories
+    //        .Where(c => c.Records.Any(r => r.Date.Year == mount.Year && r.Date.Month == mount.Month))
+    //        .ToListAsync();
+    //}
+
+    public async Task<IEnumerable<CategoryWithAmontDto>> GetAllByMountAsync(DateTime mount)
     {
-        return await _context.Categories
-            .Where(c => c.Records.Any(r => r.Date.Year == mount.Year && r.Date.Month == mount.Month))
+        var categories = await _context.Categories
+            .Include(c => c.Records.Where(r=>r.Date.Year==mount.Year && r.Date.Month==mount.Month))
             .ToListAsync();
+        return _mapper.Map<List<CategoryWithAmontDto>>(categories);
     }
 
     public Task<Category> GetByIdAsync(int id)
@@ -30,9 +42,10 @@ public class CategoryServices:ICategoryServices
         throw new NotImplementedException();
     }
 
-    public Task AddAsync(Category category)
+    public async Task AddAsync(Category category)
     {
-        throw new NotImplementedException();
+        await _context.Categories.AddAsync(category);
+        await _context.SaveChangesAsync();
     }
 
     public Task UpdateAsync(Category category)
